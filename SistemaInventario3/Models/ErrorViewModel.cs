30294de2cdeaabfe5bf8c003bee94ea672b5b43f
David <david.gonzaruiz@gmail.com>
2020-09-15 18:23:18 -0300

    Agregar archivos de proyecto.

---
+++ SistemaInventario3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaInventario3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
